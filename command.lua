7f4ee47b15743ae307754847f043350db05956b6
LuizFcc <131479466+LuizFcc@users.noreply.github.com>
2025-11-28 10:53:40 -0300

    Update command.lua

--- command.lua
+++ command.lua
@@ -6,10 +6,10 @@ core.register_chatcommand("broadcast", {
     privs={broadcast=true},
     func = function(name, param)
       local red = core.get_color_escape_sequence("#f44336")
-      local reset = core.get_color_escape_sequence("")
+      local reset = core.get_color_escape_sequence("#ffffff")
       
       -- If no player, then assume it as server
-      if not name or name == nil
+      if not name or name == nil then
         core.chat_send_all(red .. S("[Server Broadcast] ") .. reset .. param)
       else
         -- If the player exists, show the player name
