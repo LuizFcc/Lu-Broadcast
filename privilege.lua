f1f56278797d250c295a161cfd0cc36c58b355e3
LuizFcc <luizfelipecarvalhocostacosta@gmail.com>
2025-11-28 15:10:31 +0000

    Reset HEAD to 37b5826

--- privilege.lua
+++ privilege.lua
@@ -1,5 +1,3 @@
-local S = core.get_translator("lu_broadcast")
-
 core.register_privilege("broadcast", {
     give_to_singleplayer = false,
     description = S("Anyone with this privilege will be able to execute /broadcast!")
