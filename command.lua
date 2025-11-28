e9299c40947d27778a1c97c9d03a253b72e1ac25
LuizFcc <131479466+LuizFcc@users.noreply.github.com>
2025-11-28 08:37:21 -0300

    Update command.lua

--- command.lua
+++ command.lua
@@ -3,5 +3,8 @@ core.register_chatcommand("broadcast", {
     description = S("Sends a global message for everyone in the server."),
     privs={broadcast=true},
     func = function(name, param)
-      
+      local red = core.get_color_escape_sequence("#f44336")
+      local reset = core.get_color_escape_sequence("")
+      core.chat_send_all(red .. S("[Broadcast from ") .. name .. "] " .. param)
+    end
 })
