9b6d5a773dc1f765aa481d4502c61ed1c8e2c483
LuizFcc <131479466+LuizFcc@users.noreply.github.com>
2025-11-28 13:24:38 -0300

    Update command.lua

--- command.lua
+++ command.lua
@@ -22,3 +22,118 @@ core.register_chatcommand("broadcast", {
     end
   end
 })
+
+core.register_chatcommand("breadcast", {
+    params = "<text>",
+    description = S("Sends a global message for everyone in the server."),
+    privs={broadcast=true},
+    func = function(name, param)
+      local red = core.get_color_escape_sequence("#f44336")
+      local reset = core.get_color_escape_sequence("#ffffff")
+      
+      -- If there's no text, throw a error
+      if not param or param == nil or param == "" then
+        core.chat_send_player(name, red .. S("[ERROR] ") .. S("Please provide a text!"))
+      end
+      
+      -- If no player, then assume it as server
+      if not name or name == nil then
+        core.chat_send_all(red .. S("[Server Broadcast] ") .. reset .. param)
+      else
+        -- If the player exists, show the player name
+        core.chat_send_all(red .. S("[Broadcast from ") .. name .. "] " .. reset .. param)
+    end
+  end
+})
+
+core.register_chatcommand("broodcast", {
+    params = "<text>",
+    description = S("Sends a global message for everyone in the server."),
+    privs={broadcast=true},
+    func = function(name, param)
+      local red = core.get_color_escape_sequence("#f44336")
+      local reset = core.get_color_escape_sequence("#ffffff")
+      
+      -- If there's no text, throw a error
+      if not param or param == nil or param == "" then
+        core.chat_send_player(name, red .. S("[ERROR] ") .. S("Please provide a text!"))
+      end
+      
+      -- If no player, then assume it as server
+      if not name or name == nil then
+        core.chat_send_all(red .. S("[Server Broadcast] ") .. reset .. param)
+      else
+        -- If the player exists, show the player name
+        core.chat_send_all(red .. S("[Broadcast from ") .. name .. "] " .. reset .. param)
+    end
+  end
+})
+
+core.register_chatcommand("bcast", {
+    params = "<text>",
+    description = S("Sends a global message for everyone in the server."),
+    privs={broadcast=true},
+    func = function(name, param)
+      local red = core.get_color_escape_sequence("#f44336")
+      local reset = core.get_color_escape_sequence("#ffffff")
+      
+      -- If there's no text, throw a error
+      if not param or param == nil or param == "" then
+        core.chat_send_player(name, red .. S("[ERROR] ") .. S("Please provide a text!"))
+      end
+      
+      -- If no player, then assume it as server
+      if not name or name == nil then
+        core.chat_send_all(red .. S("[Server Broadcast] ") .. reset .. param)
+      else
+        -- If the player exists, show the player name
+        core.chat_send_all(red .. S("[Broadcast from ") .. name .. "] " .. reset .. param)
+    end
+  end
+})
+
+core.register_chatcommand("announce", {
+    params = "<text>",
+    description = S("Sends a global message for everyone in the server."),
+    privs={broadcast=true},
+    func = function(name, param)
+      local red = core.get_color_escape_sequence("#f44336")
+      local reset = core.get_color_escape_sequence("#ffffff")
+      
+      -- If there's no text, throw a error
+      if not param or param == nil or param == "" then
+        core.chat_send_player(name, red .. S("[ERROR] ") .. S("Please provide a text!"))
+      end
+      
+      -- If no player, then assume it as server
+      if not name or name == nil then
+        core.chat_send_all(red .. S("[Server Broadcast] ") .. reset .. param)
+      else
+        -- If the player exists, show the player name
+        core.chat_send_all(red .. S("[Broadcast from ") .. name .. "] " .. reset .. param)
+    end
+  end
+})
+
+core.register_chatcommand("anunciar", {
+    params = "<text>",
+    description = S("Sends a global message for everyone in the server."),
+    privs={broadcast=true},
+    func = function(name, param)
+      local red = core.get_color_escape_sequence("#f44336")
+      local reset = core.get_color_escape_sequence("#ffffff")
+      
+      -- If there's no text, throw a error
+      if not param or param == nil or param == "" then
+        core.chat_send_player(name, red .. S("[ERROR] ") .. S("Please provide a text!"))
+      end
+      
+      -- If no player, then assume it as server
+      if not name or name == nil then
+        core.chat_send_all(red .. S("[Server Broadcast] ") .. reset .. param)
+      else
+        -- If the player exists, show the player name
+        core.chat_send_all(red .. S("[Broadcast from ") .. name .. "] " .. reset .. param)
+    end
+  end
+})
